3c7458359a13e52b01d3e7d885e54e0552f200a1
Ori Haviv <orihaviv@gmail.com>
2017-12-29 19:57:03 +0200

    Adding the framework and the code I used for getting the Songkick data Home page and Events page: step 1

---
+++ template/events_js.js
@@ -0,0 +1,13 @@
+function openQuery(queryNum, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    // Show the specific tab content
+    document.getElementById(queryNum).style.display = "block";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
