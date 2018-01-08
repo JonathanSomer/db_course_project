43c27fc2a1cef807a93c5732d6c5ff9a3139b9fc
Jonathan Somer <jonathan.somer@fiverr.com>
2018-01-08 16:53:38 +0200

    work on query 2 not finished. waiting for limit

--- main.py
+++ main.py
@@ -195,6 +195,8 @@ def top_10_events_in_month_X(year_month):
 
     return jsonify({'events' : r})
 
+
+
 @app.route('/similar_artists_events/<string:artist>')
 def similar_artists_events(artist):
     print('GET similar artists events %s' % (artist))
@@ -215,9 +217,69 @@ def reviews_by_artist(artist):
     print('GET reviews by artist %s' % (artist))
     return jsonify(reviews)
 
-@app.route('/most_genre_city/<int:month_number>')
-def most_genre_city(month_number):
-    print('GET most genre city in month: %d' % (month_number))
+
+# Query 2: 
+@app.route('/most_genre_city/<string:year_month>')
+def most_genre_city(year_month):
+    year, month = year_month.split('-')
+
+    cur = mysql.connection.cursor()
+    query = """
+		SELECT country, 
+		       city, 
+		       genres 
+		FROM   (SELECT city, 
+		               country, 
+		               Count(*)            AS number_of_genres, 
+		               Group_concat(genre) AS Genres 
+		        FROM   (SELECT DISTINCT city, 
+		                                country, 
+		                                genre 
+		                FROM   eventim, 
+		                       artists_events, 
+		                       artist_genre, 
+		                       venues 
+		                WHERE  eventim.event_id = artists_events.event_id 
+		                       AND eventim.venue_id = venues.venue_id 
+		                       AND Month(eventim.event_date) = {0}
+		                       AND Year(eventim.event_date) = {1} 
+		                       AND artists_events.artist_id = artist_genre.artist_id) AS 
+		               city_genre 
+		        GROUP  BY city, 
+		                  country 
+		        ORDER  BY number_of_genres DESC) AS countGenres 
+		WHERE  number_of_genres = (SELECT Max(number_of_genres) 
+		                           FROM   (SELECT city, 
+		                                          country, 
+		                                          Count(*) AS number_of_genres 
+		                                   FROM   (SELECT DISTINCT city, 
+		                                                           country, 
+		                                                           genre 
+		                                           FROM   eventim, 
+		                                                  artists_events, 
+		                                                  artist_genre, 
+		                                                  venues 
+		                                           WHERE  eventim.event_id = 
+		                                                  artists_events.event_id 
+		                                                  AND eventim.venue_id = 
+		                                                      venues.venue_id 
+		                                                  AND Month(eventim.event_date) 
+		                                                      = {0} 
+		                                                  AND Year(eventim.event_date) = 
+		                                                      {1} 
+		                                                  AND artists_events.artist_id = 
+		                                                      artist_genre.artist_id) 
+		                                          AS 
+		                                          city_genre 
+		                                   GROUP  BY city, 
+		                                             country 
+		                                   ORDER  BY number_of_genres DESC) AS 
+		                                  countGenres) 
+    """.format(month, year)
+    cur.execute(query)
+    r = [dict((cur.description[i][0], value)
+              for i, value in enumerate(row)) for row in cur.fetchall()]
+
     return jsonify(city_data_genres)
 
 @app.route('/most_events_city/<int:month_number>')
